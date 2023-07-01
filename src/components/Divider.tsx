ea71f531a61d6d3ee6d360f692d93907cc8fb198
diegoHiato <diegofreitas_oliveira@proton.me>
2023-07-01 16:39:25 -0300

    Add rest prop to Divider Component

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -12,12 +12,10 @@ interface DividerProps {
 /**
  * Receive a spacing in rem to TOP and BOTTOM margins
  *
- * Example: 2rem > 32px
- *
  * Default: 1.5
  *
  * @param spacing
  */
-export const Divider = ({ spacing = 1.5 }: DividerProps) => {
-  return <HrElement $spacing={spacing} />
+export const Divider = ({ spacing = 1.5, ...rest }: DividerProps) => {
+  return <HrElement $spacing={spacing} {...rest} />
 }
